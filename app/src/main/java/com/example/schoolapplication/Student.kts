62d292dc0db84665ce7075ce69a9d382ae9d630a
engreemmnsba <reem.mns.ba@gmail.com>
2025-10-19 15:32:15 +0300

    Initial commit: SchoolApplication (Kotlin + Compose)

---
+++ app/src/main/java/com/example/schoolapplication/Student.kts
@@ -0,0 +1,54 @@
+class Student(
+    val idNumber:String ,
+    val firstName:String ,
+    val birthDate:String ,
+    val adressbegore : String ,
+    val adressAfter : String ,
+    val telphoneNumer : String ,
+    val alttelphoneNumber : String ,
+    val note : String ``    1   1111111111
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                `````````````````````````
+
+
+)
